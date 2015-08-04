2f0d5603c32a8a243ae4252e0a04b261f044f4bf
q-key <vadim.korochkin@ya.ru>
2015-08-04 15:04:33 +0600

    API date settings.

---
+++ src/ENTech.Store/ENTech.Store.Api/App_Start/CustomDateTimeConverter.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENTech.Store.Api.App_Start
+{
+    class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter()
+        {
+            // base.DateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";// "yyyy-MM-dd";
+            //base.DateTimeStyles = System.Globalization.DateTimeStyles.AdjustToUniversal;
+            //base.DateTimeStyles = System.Globalization.DateTimeStyles.None;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var v = reader.Value;
+
+            if (v.GetType() == typeof(DateTime))
+            {
+                var d = (DateTime)v;
+                if (d.Kind == DateTimeKind.Unspecified)
+                    d = new DateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second, DateTimeKind.Utc);
+                else if (d.Kind == DateTimeKind.Local)
+                    d = d.ToUniversalTime();
+                return d;
+            }
+            else if (v.GetType() == typeof(string))
+            {
+                var s = v.ToString();
+                if (s.Contains("T"))
+                {
+                    var parts = s.Split('T');
+                    var datePart  = parts[0];
+                    var timePart  = parts[1];
+                    var timeParts = parts[1].Split(':');
+                    var hh = timeParts.Length > 0 && !string.IsNullOrWhiteSpace(timeParts[0])? Convert.ToInt32(timeParts[0]) : 0;
+                    var mm = timeParts.Length > 1 && !string.IsNullOrWhiteSpace(timeParts[1])? Convert.ToInt32(timeParts[1]) : 0;
+                    var ss = timeParts.Length > 2 && !string.IsNullOrWhiteSpace(timeParts[2])? Convert.ToInt32(timeParts[2]) : 0;
+
+                    var d = DateTime.ParseExact(datePart, "yyyy-MM-dd", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+
+                    var result = new DateTime(d.Year, d.Month, d.Day, hh, mm, ss, DateTimeKind.Utc);
+                    return result;
+                }
+            }
+
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var d = (DateTime)value;
+            var v = d.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            writer.WriteValue(v);
+            //base.WriteJson(writer, value, serializer);
+        }
+    }
+
+    class MyDateTimeConvertor : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ENTech.Store/ENTech.Store.Api/App_Start/WebApiConfig.cs
+++ src/ENTech.Store/ENTech.Store.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using ENTech.Store.Api.App_Start;
 
 namespace ENTech.Store.Api
 {
@@ -14,6 +18,28 @@ namespace ENTech.Store.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //                // Create Json.Net formatter serializing DateTime using the ISO 8601 format
+            //    JsonSerializerSettings serializerSettings = new JsonSerializerSettings();            
+            //    serializerSettings.Converters.Add(new IsoDateTimeConverter());
+
+            //    GlobalConfiguration.Configuration.Formatters.Add(new System.Net.Http.Formatting.MediaTypeFormatter) = new JsonNetFormatter(serializerSettings
+            //    config.Formatters.
+
+
+            JsonMediaTypeFormatter jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            JsonSerializerSettings jSettings = new Newtonsoft.Json.JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+               // DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+               // DateFormatString = "yyyy-MM-dd'T'HH:mm:ss'Z"
+            };
+
+            //jSettings.DateTimeZoneHandling = DateTimeZoneHandling.;
+            //jSettings.DateParseHandling = DateParseHandling.
+            jSettings.Converters.Add(new CustomDateTimeConverter());
+            jsonFormatter.SerializerSettings = jSettings;
+
         }
     }
 }
